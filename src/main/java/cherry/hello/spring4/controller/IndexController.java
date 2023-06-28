dcc53542b3d80078054069505a2c707a686960c8
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-28 11:39:10 +0900

    Tiles3 실행 확인

--- src/main/java/cherry/hello/spring4/controller/IndexController.java
+++ src/main/java/cherry/hello/spring4/controller/IndexController.java
@@ -25,7 +25,8 @@ public class IndexController {
         //로거 출력
         logger.info("Hello, World!! from Logger!");
 
-        return "index";
+       // return "index";  // jsp view resolver
+        return "index.tiles";
     }
 
 
