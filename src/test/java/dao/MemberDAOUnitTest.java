0bc8461a6f24fe0ce36936979c616afccb20e7f1
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-29 15:16:44 +0900

    MemberDAO loginMember 단위 테스트

--- src/test/java/dao/MemberDAOUnitTest.java
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/servlet-context.xml",
@@ -28,6 +29,21 @@ public class MemberDAOUnitTest {
                 null, "abc123a","987xyz","abc123","abc123@987xyz.co.kr",null);
         //assertEquals(테스트메서드, 검증값)
        assertEquals(mdao.insertMember(m), 1);
+    }
+
+    @Test
+    public void loginMember() throws Exception {
+        Member m = new Member();
+        m.setUserid("abc123a");
+        m.setPasswd("987xyz");
+
+        //assertEquals(테스트메서드, 검증값)
+        // System.out.println(mdao.loginMember(m));
+
+        //abc123a 가 들어가면 expected 1, actual 0 왜냐면 없는 값이니까
+
+        assertNotNull(mdao.loginMember(m));
+
     }
 
 }
