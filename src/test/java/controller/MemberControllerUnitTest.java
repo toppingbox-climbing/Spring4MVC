f3940b90a4366ec47d429f601263258508adb4e3
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-29 15:18:42 +0900

    MemberController loginOk 단위 테스트

--- src/test/java/controller/MemberControllerUnitTest.java
+++ src/test/java/controller/MemberControllerUnitTest.java
@@ -59,6 +59,14 @@ public class MemberControllerUnitTest {
                         .param("email","abc123@987xyz.co.kr") )
                 .andExpect(redirectedUrl("/member/login"));
     }
+
+    @Test
+    public void loginokTest() throws Exception {
+        mockMvc.perform(post("/member/login")
+                        .param("userid","abc123")
+                        .param("passwd","987xyz"))
+               .andExpect(redirectedUrl("/member/myinfo"));
+    }
 }
 
 
