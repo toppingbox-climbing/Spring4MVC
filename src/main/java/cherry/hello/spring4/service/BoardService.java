d3441e5b13da776e75781293ad653d5f9dea5aa0
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-30 16:53:29 +0900

    BoardService : readBoard 구현

---
+++ src/main/java/cherry/hello/spring4/service/BoardService.java
@@ -0,0 +1,11 @@
+package cherry.hello.spring4.service;
+
+import cherry.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<Board> readBoard(int cpg);
+
+}
---
+++ src/main/java/cherry/hello/spring4/service/BoardServiceImpl.java
@@ -0,0 +1,20 @@
+package cherry.hello.spring4.service;
+
+import cherry.hello.spring4.dao.BoardDAO;
+import cherry.hello.spring4.model.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("bsrv")
+public class BoardServiceImpl implements BoardService  {
+
+    @Autowired BoardDAO bdao;
+    @Override
+    public List<Board> readBoard(int cpg) {
+        int snum = (cpg - 1) * 15;
+
+        return bdao.selectBoard(snum);
+    }
+}
