b8430bb9f149d5099538b14d284eab4058bbf5ab
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-30 16:55:18 +0900

    BoardController : list 구현

--- src/main/java/cherry/hello/spring4/controller/BoardController.java
+++ src/main/java/cherry/hello/spring4/controller/BoardController.java
@@ -1,7 +1,9 @@
 package cherry.hello.spring4.controller;
 
+import cherry.hello.spring4.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class BoardController {
 
+
+    @Autowired    BoardService bsrv;
     private Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list(Model m) {
-       // return "index";  // jsp view resolver
+    public String list(Model m, int cpg) {
+        logger.info("board/list 호출!!");
+
+        m.addAttribute("boards", bsrv.readBoard(cpg));
+
         return "board/list.tiles";
     }
     @GetMapping("write")
-    public String write(Model m) {
+    public String write() {
 
 
         // return "index";  // jsp view resolver
         return "board/write.tiles";
     }
     @GetMapping("view")
-    public String view(Model m) {
+    public String view() {
 
 
         // return "index";  // jsp view resolver
