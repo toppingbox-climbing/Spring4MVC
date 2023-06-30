8ba111e9e7b9c6a23048b3bdf72bea31ed60b6ce
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-30 16:54:00 +0900

    BoardService : readBoard 단위 테스트

---
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -0,0 +1,38 @@
+package service;
+
+import cherry.hello.spring4.model.Board;
+import cherry.hello.spring4.model.Member;
+import cherry.hello.spring4.service.BoardService;
+import cherry.hello.spring4.service.MemberService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+        "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class BoardServiceUnitTest {
+
+    //테스트할 대상 객체를 주입받음
+    @Autowired  BoardService bsrv;
+
+    @Test
+
+    public void readBoard() throws Exception {
+        int cpage = 1;
+        List<Board> results = bsrv.readBoard(cpage);
+
+        assertEquals(results.size(), 15);
+        System.out.println(results);
+    }
+}
