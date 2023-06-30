3ee197ddd24c5e5efd4b7a1a5f63e5e7ccba41c1
cherry <just.come.as.you.are.babe@gmail.com>
2023-06-30 16:52:55 +0900

    BoardDAO : selectBoard 단위 테스트

---
+++ src/test/java/dao/BoardDAOUnitTest.java
@@ -0,0 +1,35 @@
+package dao;
+
+import cherry.hello.spring4.dao.BoardDAO;
+import cherry.hello.spring4.model.Board;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+        "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+
+public class BoardDAOUnitTest {
+
+    @Autowired BoardDAO bdao;
+
+    @Test
+    public void selectBoard() throws Exception {
+        int cpage = 3;
+        int snum = (cpage - 1) * 15;
+        List<Board> results = bdao.selectBoard(snum);
+
+        assertEquals(results.size(),15);
+        System.out.println(results);
+    }
+
+}
