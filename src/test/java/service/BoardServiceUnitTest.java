1d4a7dd1d041ed80cd19cded19289d2a3b5951c4
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-03 11:13:11 +0900

    boarService : reaOneBoard 단위테스트

--- src/test/java/service/BoardServiceUnitTest.java
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -35,4 +35,14 @@ public class BoardServiceUnitTest {
         assertEquals(results.size(), 15);
         System.out.println(results);
     }
+
+    @Test
+    public void readOneBoard() throws Exception {
+        String bno= "450";
+        Board result = bsrv.readOneBoard(bno);
+
+        assertNotNull(result);
+        System.out.println(result);
+
+    }
 }
