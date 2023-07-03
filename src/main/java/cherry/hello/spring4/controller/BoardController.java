4017ff6db0f58241410b4018f01597f4a39a360d
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-03 14:54:07 +0900

    boardController : writeok 구현

--- src/main/java/cherry/hello/spring4/controller/BoardController.java
+++ src/main/java/cherry/hello/spring4/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -36,6 +37,17 @@ public class BoardController {
         // return "index";  // jsp view resolver
         return "board/write.tiles";
     }
+    @PostMapping("/write")
+    public String writeok(Board bd) {
+        String returnPage = "redirect:/board/fail";
+
+        if (bsrv.saveBoard(bd))
+            returnPage = "redirect:/board/list?cpg=1";
+
+        return returnPage;
+    }
+
+
     @GetMapping("view")
     public String view(Model m,String bno) {
         logger.info("board/view 호출!!");
